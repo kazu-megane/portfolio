2187ff61d359f23e2d4ec4810a8f703c404c4d21
kazu-megane <kazu.megane.h@gmail.com>
2025-09-10 23:56:25 +0900

    [fix]topページを整備

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,206 @@
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+const MENU_DATA = [
+  { title: 'ABOUT', href: '#' },
+  { title: 'WORKS', href: '#' },
+  { title: 'ARCHIVE', href: '#' },
+  { title: 'CONTACT', href: '#' },
+];
+
+const MENU_ID = 'menu';
+
+const SectionHeading = ({ text }: { text: string }) => {
+  return (
+    <div className="sticky top-0 z-10 bg-white px-2 py-6 text-stone-600 md:px-4">
+      <p className="text-center font-serif text-base tracking-[-.02em] md:text-left md:text-lg">{text}</p>
+    </div>
+  );
+};
+
+const SectionText = ({ text, className }: { text: string; className?: string }) => {
+  return (
+    <p className={twMerge('text-center text-xs whitespace-pre-line text-stone-600 md:text-sm', className)}>{text}</p>
+  );
+};
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <ol className="list-inside list-decimal text-center font-mono text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-mono font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-        </ol>
+  const [blurValue, setBlurValue] = useState(8); // Initial blur value in pixels
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm font-medium text-background transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const newBlurValue = Math.max(0, 8 - scrollY / 50); // Decrease blur as user scrolls
+      setBlurValue(newBlurValue);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.querySelector('body')?.classList.add('overflow-hidden');
+    } else {
+      document.querySelector('body')?.classList.remove('overflow-hidden');
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <div className="flex flex-col bg-white">
+      <main className="flex grow flex-col">
+        <div className="grid h-screen grow place-items-center">
+          <div className="fixed z-20 mb-6 flex w-full px-4 font-serif text-xs text-white mix-blend-difference sm:px-6">
+            <h1 className="grow tracking-[.2em]">
+              <button
+                type="button"
+                onClick={() => {
+                  window.scrollTo({ top: 0, behavior: 'smooth' });
+                }}
+                className="cursor-pointer"
+              >
+                KAZUYA HASHIMOTO
+              </button>
+            </h1>
+            <ul className="hidden items-center gap-[80px] tracking-[-.02em] md:flex lg:gap-[120px]">
+              {MENU_DATA.map((item) => (
+                <li key={item.title}>
+                  <Link href={item.href} className="transition-opacity ease-in hover:opacity-50">
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <button
+              type="button"
+              aria-controls={MENU_ID}
+              aria-expanded={isMenuOpen}
+              aria-haspopup="true"
+              aria-label="Open menu"
+              onClick={() => {
+                setIsMenuOpen(true);
+              }}
+              className="absolute top-[50%] right-2 -translate-y-1/2 transform cursor-pointer p-3 tracking-[-.02em] transition-opacity ease-in hover:opacity-50 sm:right-3 md:hidden"
+            >
+              MENU
+            </button>
+          </div>
+          <div
+            id={MENU_ID}
+            role="menu"
+            className={twMerge(
+              'fixed top-0 left-0 z-30 flex h-full w-full items-center backdrop-blur-xl transition-opacity duration-300 ease-in-out',
+              isMenuOpen ? 'opacity-100' : 'pointer-events-none opacity-0',
+            )}
+            onClick={() => {
+              setIsMenuOpen(false);
+            }}
           >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <ul className="mb-6 flex flex-col gap-4">
+              {MENU_DATA.map((item, index) => (
+                <li
+                  key={item.title}
+                  className={twMerge(
+                    'p-4 text-center transition-all duration-300 ease-in-out',
+                    isMenuOpen ? 'opacity-100' : 'opacity-0',
+                  )}
+                  style={{
+                    transitionDelay: isMenuOpen ? `${index * 100}ms` : `${(MENU_DATA.length - 1 - index) * 100}ms`,
+                  }}
+                >
+                  <Link
+                    href={item.href}
+                    className="font-serif text-lg text-stone-800"
+                    onClick={(event) => {
+                      event.stopPropagation();
+                    }}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="relative mb-6 grow px-4 text-right">
+              <button className="absolute top-[50%] right-2 -translate-y-1/2 cursor-pointer p-3 font-serif text-xs text-stone-600">
+                CLOSE
+              </button>
+            </div>
+          </div>
+          <div className="relative h-full w-full overflow-hidden">
+            <Image
+              src="/top_main.jpg"
+              alt=""
+              priority={true}
+              fill={true}
+              objectFit="cover"
+              loading="eager"
+              className="scale-110"
+              style={{ filter: `blur(${blurValue}px)` }}
+            />
+          </div>
+        </div>
+        <div className="pb-20">
+          <div className="mx-4 md:mx-0">
+            <SectionHeading text="Snapshot" />
+            <div className="grid grid-cols-1 gap-8 md:gap-8">
+              <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 md:gap-6">
+                <Image
+                  src="/top_snap_1.jpg"
+                  width={3840}
+                  height={2539}
+                  alt=""
+                  className="aspect-[3/2] object-contain"
+                />
+                <SectionText text={'「 」\nISO 200, F4.0, 1/500s'} />
+              </div>
+              <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 md:gap-6">
+                <Image
+                  src="/top_snap_2.jpg"
+                  width={552}
+                  height={368}
+                  alt=""
+                  className="aspect-[3/2] h-full w-full object-contain md:col-start-2 md:row-start-1"
+                />
+                <SectionText text={'「 」\nISO 200, F4.0 ,1/90s, 28mm'} className="md:col-start-1 md:row-start-1" />
+              </div>
+            </div>
+          </div>
+          <div className="mx-4 mt-8 md:mx-0">
+            <SectionHeading text="Abstract" />
+            <div className="grid grid-cols-1 gap-8 md:gap-8">
+              <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 md:gap-6">
+                <Image
+                  src="/top_abstract_1.jpg"
+                  width={1920}
+                  height={2881}
+                  alt=""
+                  className="aspect-[3/2] h-full w-full object-contain"
+                />
+                <SectionText text={'「 」\nISO 200, F4.0, 1/500s'} />
+              </div>
+              <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 md:gap-6">
+                <Image
+                  src="/top_abstract_2.jpg"
+                  width={2080}
+                  height={3120}
+                  alt=""
+                  className="aspect-[3/2] h-full w-full object-contain md:col-start-2 md:row-start-1"
+                />
+                <SectionText text={'「 」\nISO 200, F4.0 ,1/90s, 28mm'} className="md:col-start-1 md:row-start-1" />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
+      <footer className="fixed bottom-0 z-10 w-full p-4 text-center font-serif text-sm text-white mix-blend-difference">
+        <small>&copy; kazuya hashimoto</small>
       </footer>
     </div>
   );
