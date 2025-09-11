96a2e6161dd51f0f2133e69723fc697c34798d4f
kazu-megane <kazu.megane.h@gmail.com>
2025-09-11 12:48:07 +0900

    [fix]アクセシビリティ改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const MENU_ID = 'menu';
 const SectionHeading = ({ text }: { text: string }) => {
   return (
     <div className="sticky top-0 z-10 bg-white px-2 py-6 text-stone-600 md:px-4">
-      <p className="text-center font-serif text-base tracking-[-.02em] md:text-left md:text-lg">{text}</p>
+      <h2 className="text-center font-serif text-base tracking-[-.02em] md:text-left md:text-lg">{text}</h2>
     </div>
   );
 };
@@ -94,7 +94,12 @@ export default function Home() {
     <div className="flex flex-col bg-white">
       <main className="flex grow flex-col">
         <div className="grid h-screen grow place-items-center">
-          <div className="fixed z-20 mb-6 flex w-full px-4 font-serif text-xs text-white mix-blend-difference sm:px-6">
+          <div
+            className={twMerge(
+              'fixed z-20 mb-6 w-full px-4 font-serif text-xs text-white mix-blend-difference sm:px-6',
+              isMenuOpen ? 'hidden' : 'flex',
+            )}
+          >
             <h1 className="grow tracking-[.2em]">
               <button
                 type="button"
@@ -106,15 +111,17 @@ export default function Home() {
                 KAZUYA HASHIMOTO
               </button>
             </h1>
-            <ul className="hidden items-center gap-[80px] tracking-[-.02em] md:flex lg:gap-[120px]">
-              {MENU_DATA.map((item) => (
-                <li key={item.title}>
-                  <Link href={item.href} className="transition-opacity ease-in hover:opacity-50">
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <nav aria-label="メイン">
+              <ul className="hidden items-center gap-[80px] tracking-[-.02em] md:flex lg:gap-[120px]">
+                {MENU_DATA.map((item) => (
+                  <li key={item.title}>
+                    <Link href={item.href} className="transition-opacity ease-in hover:opacity-50">
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
             <button
               type="button"
               aria-controls={MENU_ID}
@@ -131,39 +138,40 @@ export default function Home() {
           </div>
           <div
             id={MENU_ID}
-            role="menu"
             className={twMerge(
               'fixed top-0 left-0 z-30 flex h-full w-full items-center backdrop-blur-xl transition-opacity duration-300 ease-in-out',
-              isMenuOpen ? 'opacity-100' : 'pointer-events-none opacity-0',
+              isMenuOpen ? 'opacity-100' : 'invisible opacity-0',
             )}
             onClick={() => {
               setIsMenuOpen(false);
             }}
           >
-            <ul className="mb-6 flex flex-col gap-4">
-              {MENU_DATA.map((item, index) => (
-                <li
-                  key={item.title}
-                  className={twMerge(
-                    'p-4 text-center transition-all duration-300 ease-in-out',
-                    isMenuOpen ? 'opacity-100' : 'opacity-0',
-                  )}
-                  style={{
-                    transitionDelay: isMenuOpen ? `${index * 100}ms` : `${(MENU_DATA.length - 1 - index) * 100}ms`,
-                  }}
-                >
-                  <Link
-                    href={item.href}
-                    className="font-serif text-lg text-stone-800"
-                    onClick={(event) => {
-                      event.stopPropagation();
+            <nav className="mb-6 flex flex-col gap-4" aria-label="メイン">
+              <ul>
+                {MENU_DATA.map((item, index) => (
+                  <li
+                    key={item.title}
+                    className={twMerge(
+                      'p-4 text-center transition-all duration-300 ease-in-out',
+                      isMenuOpen ? 'opacity-100' : 'opacity-0',
+                    )}
+                    style={{
+                      transitionDelay: isMenuOpen ? `${index * 100}ms` : `${(MENU_DATA.length - 1 - index) * 100}ms`,
                     }}
                   >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+                    <Link
+                      href={item.href}
+                      className="font-serif text-lg text-stone-800"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                      }}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
             <div className="relative mb-6 grow px-4 text-right">
               <button className="absolute top-[50%] right-2 -translate-y-1/2 cursor-pointer p-3 font-serif text-xs text-stone-600">
                 CLOSE
@@ -193,7 +201,7 @@ export default function Home() {
                     src="/top_snap_1.jpg"
                     width={3840}
                     height={2539}
-                    alt=""
+                    alt="untitled snapshot"
                     className="aspect-[3/2] object-contain"
                   />
                 </AnimatedWrapper>
@@ -205,7 +213,7 @@ export default function Home() {
                     src="/top_snap_2.jpg"
                     width={552}
                     height={368}
-                    alt=""
+                    alt="untitled snapshot"
                     className="aspect-[3/2] h-full w-full object-contain md:col-start-2 md:row-start-1"
                   />
                 </AnimatedWrapper>
@@ -222,7 +230,7 @@ export default function Home() {
                     src="/top_abstract_1.jpg"
                     width={1920}
                     height={2881}
-                    alt=""
+                    alt="spectrum"
                     className="aspect-[3/2] h-full w-full object-contain"
                   />
                 </AnimatedWrapper>
@@ -234,7 +242,7 @@ export default function Home() {
                     src="/top_abstract_2.jpg"
                     width={2080}
                     height={3120}
-                    alt=""
+                    alt="fireworks"
                     className="aspect-[3/2] h-full w-full object-contain md:col-start-2 md:row-start-1"
                   />
                 </AnimatedWrapper>
