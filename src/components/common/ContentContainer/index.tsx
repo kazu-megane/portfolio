fe5b2beb8bd71a8a6bc9815fbaeaefa4903e0002
kazu-megane <kazu.megane.h@gmail.com>
2025-09-12 04:48:51 +0900

    [fix]余白を修正

--- src/components/common/ContentContainer/index.tsx
+++ src/components/common/ContentContainer/index.tsx
@@ -5,5 +5,5 @@ type Props = {
 };
 
 export const ContentContainer = ({ children }: Props) => {
-  return <div className="mx-4 mt-10 flex grow flex-col pb-40 md:mx-6 md:mt-20">{children}</div>;
+  return <div className="mx-4 mt-10 flex grow flex-col pb-25 md:mx-6 md:mt-20 md:pb-40">{children}</div>;
 };
